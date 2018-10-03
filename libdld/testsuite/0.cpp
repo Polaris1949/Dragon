1015599e101c485ae820d7eab9156b7209881239
Polaris1949 <Polaris1949@126.com>
2018-10-03 16:19:04 +0800

    New test

---
+++ libdld/testsuite/0.cpp
@@ -0,0 +1,25 @@
+#include <libdld.h>
+#define URL_ROOT "http://res.17roco.qq.com/res/combat/"
+
+int main()
+{
+    dld::manager mgr;
+
+    // Task #1
+    mgr.push(dld::task{URL_ROOT "previews/11698-idle.swf", "~/0.swf"});
+
+    // Task #2
+    mgr.push(dld::task{URL_ROOT, "~/403.txt"});
+
+    // Task #3
+    mgr.push(dld::task{URL_ROOT "previews/0-idle.swf", "~/404.swf"});
+
+    // Only execute #1 and #2, then exit because of a 403 error in #2
+    mgr.execute();
+
+    // Task #4
+    mgr.push(dld::task{URL_ROOT "spirits/11698-.swf", "~/1.swf"});
+
+    // Execute all remaining tasks, no matter there was a 404 error in #3
+    mgr.execute_all();
+}
