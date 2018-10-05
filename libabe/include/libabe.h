b45cb2bccc990b75ba5a3bc455d42dc338b4a48a
Polaris1949 <Polaris1949@126.com>
2018-10-05 22:48:45 +0800

    Implement libabe

---
+++ libabe/include/libabe.h
@@ -0,0 +1,11 @@
+#ifndef _LIBABE_H
+#define _LIBABE_H 1
+
+#include <abe/attribute.h>
+#include <abe/database.h>
+#include <abe/file.h>
+#include <abe/relation.h>
+#include <abe/translator.h>
+#include <abe/version.h>
+
+#endif
