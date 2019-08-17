4ad50fc886562e9dc5623c3083133419a2988d04
Polaris1949 <Polaris1949@126.com>
2019-08-17 15:10:37 +0800

    Fix

--- libdnl/testsuite/0.cc
+++ libdnl/testsuite/0.cc
@@ -3,22 +3,22 @@
 
 int main()
 {
-    dld::manager mgr;
+    dnl::manager mgr;
 
     // Task #1
-    mgr.push(dld::task{URL_ROOT "previews/11698-idle.swf", "~/0.swf"});
+    mgr.push(dnl::task{URL_ROOT "previews/11698-idle.swf", "~/0.swf"});
 
     // Task #2
-    mgr.push(dld::task{URL_ROOT, "~/403.txt"});
+    mgr.push(dnl::task{URL_ROOT, "~/403.txt"});
 
     // Task #3
-    mgr.push(dld::task{URL_ROOT "previews/0-idle.swf", "~/404.swf"});
+    mgr.push(dnl::task{URL_ROOT "previews/0-idle.swf", "~/404.swf"});
 
     // Only execute #1 and #2, then exit because of a 403 error in #2
     mgr.execute();
 
     // Task #4
-    mgr.push(dld::task{URL_ROOT "spirits/11698-.swf", "~/1.swf"});
+    mgr.push(dnl::task{URL_ROOT "spirits/11698-.swf", "~/1.swf"});
 
     // Execute all remaining tasks, no matter there was a 404 error in #3
     mgr.execute_all();
